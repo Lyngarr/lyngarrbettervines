54f48c48b00707158b7cf5c0fb4ee44cafe0ae95
João G. Carvalho <joaoseisei@gmail.com>
2024-11-23 17:21:22 -0300

    adiciona inserçao de item inicial

---
+++ src/main/java/com/example/Itens.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import net.minecraft.item.Item;
+
+public class Itens {
+
+    public static Item COBRE_REFORCADO = Utilidades.registrarItem("cobre_reforcado");
+
+
+    public static void Inicializa(){}
+}
