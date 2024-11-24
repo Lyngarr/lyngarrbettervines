2462da22bba1534df58a8f6d5a189b778e215172
DiceRunner714 <eduardo.sandes6@gmail.com>
2024-11-24 13:41:43 -0300

    add conserta classes Blocos e RegistrarBloco

--- src/main/java/com/example/RegistrarBlocos.java
+++ src/main/java/com/example/RegistrarBlocos.java
@@ -1,36 +1,42 @@
 package com.example;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
 
 public class RegistrarBlocos {
 
-    private static final String nomeMod = "logicthinkering";
+    public static Block registrarBloco(String id, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
+        RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ExampleMod.nomeMod, id));
 
-    private static RegistryKey<Item> keyOf(String id) {
-        return RegistryKey.of(RegistryKeys.ITEM, Identifier.of(nomeMod, id));
+        Block bloco = factory.apply(settings.registryKey(key));
+
+        Registry.register(Registries.BLOCK, key, bloco);
+
+        registrarItemBloco(key, bloco);
+
+        return bloco;
     }
 
-    public static Block register(Block bloco, String nome, boolean deveRegistrarComoItem){
-        Identifier id = Identifier.of(nomeMod, nome);
+    private static void registrarItemBloco(RegistryKey<Block> key, Block bloco) {
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, key.getValue());
 
-        if(deveRegistrarComoItem){
-            BlockItem blockItem = new BlockItem(bloco, new Item.Settings());
-            Registry.register(Registries.ITEM, id, blockItem);
+        Item item = new BlockItem(bloco, new Item.Settings().registryKey(itemKey));
+        if (item instanceof BlockItem blockItem) {
+            blockItem.appendBlocks(Item.BLOCK_ITEMS, item);
         }
-        return Registry.register(Registries.BLOCK, id, bloco);
+
+        Registry.register(Registries.ITEM, itemKey, item);
     }
 
-   private static Block registrarBloco(Block bloco, String nome, boolean deveRegistrarComoItem){
-        return register(bloco, nome, deveRegistrarComoItem);
-    }
 }
